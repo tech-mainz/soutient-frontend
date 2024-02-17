edf5cd571df46c2e6f872118a4ee892f2a4c7458
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-02-17 20:48:01 +0530

    latest

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import reactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import App from "./App";
-import './index.css'
+import "./index.css";
+import { StateContextProvider } from "./context";
 const root = reactDom.createRoot(document.getElementById("root"));
 root.render(
   <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <BrowserRouter>
-      <App/>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </BrowserRouter>
   </ThirdwebProvider>
 );
