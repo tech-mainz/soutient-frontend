005c8dc257a11cfca18cffb2541a4b03dcf92c90
nandiinii <preethanandini175@gmail.com>
2024-02-22 13:50:12 +0530

    campaign creation form

--- src/pages/CreateCampaign/CreateCampaign.jsx
+++ src/pages/CreateCampaign/CreateCampaign.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { useContract, useContractWrite,Web3Button } from "@thirdweb-dev/react";
+import { useContract, useContractWrite, Web3Button } from "@thirdweb-dev/react";
 import { contractId } from "../../utils/urls";
 import "./CreateCampaign.css";
- 
+
 export default function CreateCampaign() {
   const { contract } = useContract(contractId);
   const { mutateAsync: createCampaign, isLoading } = useContractWrite(
@@ -34,85 +34,85 @@ export default function CreateCampaign() {
     <div className="campaign__creation_main">
       <form className="campaign__creation_form">
         <div className="form_field">
-      <label for="owner">Owner: </label>
-      <input
-        type="text"
-        id="owner"
-        // placeholder="Owner"
-        name="owner"
-        value={owner}
-        onChange={(e) => setOwner(e.target.value)}
-      />
-      </div>
-      <div className="form_field">
-      <label for="title">Title: </label>
-      <input
-        type="text"
-        id="title"
-        name="title"
-        // placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      </div>
-      <div className="form_field">
-      <label for="description">Description: </label>
-      <textarea
-        id="description"
-        name="description"
-        // placeholder="Description"
-        value={description}
-        rows={7}
-        cols={30}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      </div>
-      <div className="form_field">
-      <label for="target">Target: </label>
-      <input
-        type="number"
-        step=".01"
-        id="target"
-        name="target"
-        // placeholder="Target"
-        value={target}
-        onChange={(e) => setTarget(e.target.value)}
-      />
-      </div>
-      <div className="form_field">
-      <label for="deadline">Deadline: </label>
-      <input
-        type="datetime-local"
-        id="deadline"
-        name="deadline"
-        // placeholder="Deadline"
-        value={deadline}
-        onChange={(e) => setDeadline(e.target.value)}
-      />
-      </div>
-      <div className="form_field">
-      <label for="image">Image URL: </label>
-      <input
-        type="text"
-        id="image"
-        name="image"
-        // placeholder="Image URL"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}
-      />
-      </div>
-      <div className="campaign__creation_button">
-      <Web3Button
-      className="web3__button"
-      contractAddress={contractId}
-      action={(contract) => {
-        callCreateCampaign();
-      }}
-    >
-      createCampaign
-    </Web3Button>
-    </div>
-    </form>
+          <label>Owner: </label>
+          <input
+            type="text"
+            id=""
+            // placeholder="Owner"
+            name=""
+            value={owner}
+            onChange={(e) => setOwner(e.target.value)}
+          />
+        </div>
+        <div className="form_field">
+          <label>Title: </label>
+          <input
+            type="text"
+            id=""
+            name=""
+            // placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form_field">
+          <label>Description: </label>
+          <textarea
+            id=""
+            name=""
+            // placeholder="Description"
+            value={description}
+            rows={7}
+            cols={30}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form_field">
+          <label>Target: </label>
+          <input
+            type="number"
+            step=".01"
+            id=""
+            name=""
+            // placeholder="Target"
+            value={target}
+            onChange={(e) => setTarget(e.target.value)}
+          />
+        </div>
+        <div className="form_field">
+          <label>Deadline: </label>
+          <input
+            type="datetime-local"
+            id=""
+            name=""
+            // placeholder="Deadline"
+            value={deadline}
+            onChange={(e) => setDeadline(e.target.value)}
+          />
+        </div>
+        <div className="form_field">
+          <label>Image URL: </label>
+          <input
+            type="text"
+            id=""
+            name=""
+            // placeholder="Image URL"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </div>
+        <div className="campaign__creation_button">
+          <Web3Button
+            className="web3__button"
+            contractAddress={contractId}
+            action={(contract) => {
+              callCreateCampaign();
+            }}
+          >
+            createCampaign
+          </Web3Button>
+        </div>
+      </form>
     </div>
   );
 }
