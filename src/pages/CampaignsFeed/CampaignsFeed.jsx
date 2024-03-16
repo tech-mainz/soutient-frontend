f04cee44a8c11d49442c0680d955a56d43126aff
nandiinii <preethanandini175@gmail.com>
2024-03-16 23:53:22 +0530

    list campaign api

--- src/pages/CampaignsFeed/CampaignsFeed.jsx
+++ src/pages/CampaignsFeed/CampaignsFeed.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { contractId as contractAddress } from "../../utils/urls";
+import { Abi } from "../../utils/Abi";
+import { UserContext } from "../../contexts/UserContext";
 import "./CampaignsFeed.css";
-const CampaignsFeed = () => {
+
+export default function CampaignsFeed() {
+  const { isAuthenticated, userAddress } = useContext(UserContext);
+  const [campaigns, setCampaigns] = useState([]);
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(contractAddress, Abi, provider);
+    
+    const getCampaigns = async () => {
+      try {
+        const campaigns = await contract.getCampaigns();
+        console.log("Campaigns:", campaigns);
+        setCampaigns(campaigns);
+      } catch (error) {
+        console.error("Error fetching campaigns:", error);
+      }
+    };
+
+    if (isAuthenticated) {
+      getCampaigns();
+    }
+  }, [isAuthenticated]);
+
   return (
     <div className="campaign__feed_pg">
       <h1>List Campaigns</h1>
       <div className="campaigns__list_contain">
-        <div className="campaign__list_card">
-          <img src="https://avatars.githubusercontent.com/u/75477017?v=4" alt="" />
+      {campaigns.map((campaign, index) => (
+        <div className="campaign__list_card" key={index}>
+          <img src={campaign.image} alt="" />
           <div className="campaign__feed_card_body">
             <div className="feed_card_body_text">
-              <p>Target: 100 ETH</p>
-              <p>Current: 12 ETH</p>
+              <p><span className="tag__val">Target:</span> {campaign.target.toString()}</p>
+              <p><span className="tag__val">Current:</span> {campaign.amountCollected.toString()}</p>
             </div> 
-            <button>Donate</button>
+            <button className="submit__button">Donate</button>
           </div>
         </div>
+         ))}
       </div>
     </div>
   );
 };
-
-export default CampaignsFeed;
