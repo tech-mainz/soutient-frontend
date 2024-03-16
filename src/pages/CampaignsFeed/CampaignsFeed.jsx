6cf0bb37e621cc4daf399713589c4d9ae92ed81f
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-16 12:17:06 +0530

    css

--- src/pages/CampaignsFeed/CampaignsFeed.jsx
+++ src/pages/CampaignsFeed/CampaignsFeed.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./CampaignsFeed.css";
-// import { useContract, useContractRead } from "@thirdweb-dev/react";
-// import { contractId } from "../../utils/urls";
 const CampaignsFeed = () => {
-  // const { contract } = useContract(contractId);
-  // const { data, isLoading } = useContractRead(contract, "getCampaigns");
-  // console.log("campaigns", data);
   return (
-    <div>
+    <div className="campaign__feed_pg">
       <h1>List Campaigns</h1>
       <div className="campaigns__list_contain">
-      
+        <div className="campaign__list_card">
+          <img src="https://avatars.githubusercontent.com/u/75477017?v=4" alt="" />
+          <div className="campaign__feed_card_body">
+            <div className="feed_card_body_text">
+              <p>Target: 100 ETH</p>
+              <p>Current: 12 ETH</p>
+            </div> 
+            <button>Donate</button>
+          </div>
+        </div>
       </div>
     </div>
   );
