a443d38e28e6caade2ed85f71ff3a7b6df6afcbc
nandiinii <preethanandini175@gmail.com>
2024-03-18 19:37:28 +0530

    css

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const secretKey = import.meta.env.VITE_SECRET_KEY;
 const contractId = import.meta.env.VITE_CONTRACT_ID;
-const maticUrl="https://rpc-mumbai.maticvigil.com/"
+const maticUrl="https://polygon-mumbai.g.alchemy.com/v2/DcBnDhuJqPggFzQ4Tc3CFy2WVlzPvnTR"
 export { secretKey, clientId, contractId,maticUrl };
