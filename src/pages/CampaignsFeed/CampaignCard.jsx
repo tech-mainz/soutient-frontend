412be819119b688b7bb1c6b1fdfb508cd1107db0
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-19 10:03:21 +0530

    un comment

--- src/pages/CampaignsFeed/CampaignCard.jsx
+++ src/pages/CampaignsFeed/CampaignCard.jsx
@@ -7,33 +7,21 @@ import { contractId as contractAddress } from "../../utils/urls";
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const contract = new ethers.Contract(contractAddress, Abi, provider);
 const CampaignCard = ({campaign}) => {
-    // console.log("Campaign individual: ",campaign)
     const handleDonate = async () => {
       try {
-          // Request access to user's Metamask account
           const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
           const signer = provider.getSigner();
           const contractWithSigner = contract.connect(signer);
   
-          // Prompt user for donation amount
           let donationAmount = prompt("Enter donation amount:");
           if (donationAmount === null || donationAmount.trim() === "") {
-              // User cancelled donation or entered empty amount
               return;
           }
-  
-          // Convert donation amount to ethers format
           const ethersDonationAmount = ethers.utils.parseEther(donationAmount);
-  
-          // Make the donation transaction
           const tx = await contractWithSigner.donateToCampaign(campaign[0], {
               value: ethersDonationAmount
           });
-  
-          // Wait for transaction confirmation
           await tx.wait();
-  
-          // Display success message
           alert("Donation successful!");
       } catch (error) {
           console.error("Error donating to campaign:", error);
