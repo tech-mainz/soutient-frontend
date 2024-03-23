ab5ed28e8ee12657dd3b3501fae5ca4a6629addb
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-23 11:58:57 +0530

    dialog

--- src/pages/Upvoting/UpvotingCard.jsx
+++ src/pages/Upvoting/UpvotingCard.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Upvoting.css";
-
+import UpvoteDialog from '../../components/UpvoteDialog/UpvoteDialog';
 const UpvotingCard = () => {
+  const[open,setOpen]=useState(false)
+  const handleClose=()=>{
+    setOpen(false)
+  }
+  const handleOpen=()=>{
+    setOpen(true);
+  }
   return (
     <div className='upvoting__card'>
+      <UpvoteDialog open={open} handleClose={handleClose}/>
         <img src='https://ibeto.excelmec.org/ibetologo.png' alt='img'></img>
         <p>Name</p>
-        <button className='submit__button'>View Details</button>
+        <button className='submit__button' onClick={handleOpen}>View Details</button>
     </div>
   )
 }
