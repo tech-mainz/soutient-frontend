e30d05f833a302b821795063f99ed590d6c42ba0
nandiinii <preethanandini175@gmail.com>
2024-03-25 22:20:28 +0530

    toast msg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainPage, CampaignsFeed, CreateCampaign, Upvoting } from "./pages";
 import UserDetails from "./contexts/UserContext";
 import ConnectWalletButton from "./components/ConnectWalletButton/ConnectWalletButton";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -21,6 +23,7 @@ const App = () => {
             <Route path="/upvote" element={<Upvoting />} />
           </Routes>
         </BrowserRouter>
+        <ToastContainer />
       </UserDetails>
     </div>
   );
