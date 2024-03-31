5c156f1f1ebbf6fda98d00128aa03ef641dd8cfb
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-31 21:38:23 +0530

    loan application frontend

--- src/pages/LoanApplication/LoanApplication.jsx
+++ src/pages/LoanApplication/LoanApplication.jsx
@@ -1,35 +1,97 @@
-import React from 'react'
-import './LoanApplication.css'
-const LoanApplication = () => {
-  return (
-    <div className="student__loan__form">
-      <h1>Loan Application</h1>
-      <div className="form_field">
-        <label>Requester Name</label>
-        <input type="text" name="requestor_name"/>
+import React, { Component } from "react";
+import axios from "axios";
+import "./LoanApplication.css";
+
+class LoanApplication extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requesterName: "",
+      institutionalAddress: "",
+      need: "",
+      grade: "",
+      image: null,
+      fundNeeded: null,
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { requesterName, institutionalAddress, need, grade, image, fundNeeded } = this.state;
+    const requesterMetamaskId = window.localStorage.getItem("userAddress");
+
+    try {
+      const formData = new FormData();
+      formData.append("requester_metamask_id", requesterMetamaskId);
+      formData.append("requester_name", requesterName);
+      formData.append("institutional_address", institutionalAddress);
+      formData.append("need", need);
+      formData.append("grade", grade);
+      formData.append("image", image);
+      formData.append("fund_needed", fundNeeded);
+
+      await axios.post("http://127.0.0.1:8000/loan-request/", formData);
+
+      // Reset form fields after successful submission
+      this.setState({
+        requesterName: "",
+        institutionalAddress: "",
+        need: "",
+        grade: "",
+        image: null,
+        fundNeeded: null,
+      });
+
+      alert("Loan application submitted successfully!");
+    } catch (error) {
+      console.error("Error submitting loan application:", error);
+    }
+  };
+
+  handleImageChange = (e) => {
+    this.setState({ image: e.target.files[0] });
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { requesterName, institutionalAddress, need, grade, fundNeeded } = this.state;
+
+    return (
+      <div className="student__loan__main_pg">
+        <form className="student__loan__form" onSubmit={this.handleSubmit}>
+          <h1>Loan Application</h1>
+          <div className="form_field">
+            <label>Requester Name</label>
+            <input type="text" name="requesterName" value={requesterName} onChange={this.handleChange} />
+          </div>
+          <div className="form_field">
+            <label>Institutional Address</label>
+            <input type="text" name="institutionalAddress" value={institutionalAddress} onChange={this.handleChange} />
+          </div>
+          <div className="form_field">
+            <label>Need</label>
+            <input type="text" name="need" value={need} onChange={this.handleChange} />
+          </div>
+          <div className="form_field">
+            <label>Grade</label>
+            <input type="number" name="grade" value={grade} onChange={this.handleChange} />
+          </div>
+          <div className="form_field">
+            <label>Image</label>
+            <input type="file" id="user_image" name="image" accept=".jpg, .jpeg, .png" onChange={this.handleImageChange} />
+          </div>
+          <div className="form_field">
+            <label>Fund Needed</label>
+            <input type="number" name="fundNeeded" value={fundNeeded} onChange={this.handleChange} />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      <div className="form_field">
-        <label>Institutional Address</label>
-        <input type="address" name="address"/>
-      </div>
-      <div className="form_field">
-        <label>Need</label>
-        <input type="text" name="need"/>
-      </div>
-      <div className="form_field">
-        <label>Grade</label>
-        <input type="number" name="grade"/>
-      </div>
-      <div className="form_field">
-        <label>Image</label>
-        <input type="button" name="File" value="Choose File"/>
-      </div>
-      <div className="form_field">
-        <label>Fund Needed</label>
-        <input type="number" name="fund_needed"/>
-      </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default LoanApplication
+export default LoanApplication;
